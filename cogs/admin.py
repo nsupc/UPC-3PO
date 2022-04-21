892e66f230f9943559b5e1b7894ecb1203a24fd2
UPC-NS <87896540+UPC-NS@users.noreply.github.com>
2022-04-21 17:17:30 -0400

    working on ban/unban commands

--- cogs/admin.py
+++ cogs/admin.py
@@ -23,10 +23,10 @@ class admin(commands.Cog):
     @commands.has_permissions(manage_roles=True)
     async def addrole(self, ctx, member : commands.MemberConverter, role : discord.Role):
         await member.add_roles(role)
+        await ctx.send(f"{member.name} has been added to {role}.")
         log = self.bot.get_channel(get_log(ctx.guild.id))
         if log:
-            await log.send(f"{member} was added role '{role}' on <t:{int(time.time())}:F>")
-        await ctx.send(f"{member.name} has been added to {role}.")
+            await log.send(f"<t:{int(time.time())}:F>: {member} was added role '{role}'")
 
     @addrole.error
     async def addrole_error(self, ctx, error):
@@ -36,6 +36,8 @@ class admin(commands.Cog):
             await ctx.send("Sorry, I don't have permission to manage roles in this server.")
         elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please include both a user and a role.")
+        else:
+            await ctx.send("I can't manage roles above mine in the server's role hierarchy.")
         
     @commands.command()
     @isLoaded()
@@ -43,10 +45,10 @@ class admin(commands.Cog):
     @commands.has_permissions(manage_roles=True)
     async def remrole(self, ctx, member : commands.MemberConverter, role : discord.Role):
         await member.remove_roles(role)
+        await ctx.send(f"{member.name} has been removed from {role}.")
         log = self.bot.get_channel(get_log(ctx.guild.id))
         if log:
-            await log.send(f"{member} was removed from role '{role}' on <t:{int(time.time())}:F>")
-        await ctx.send(f"{member.name} has been removed from {role}.")
+            await log.send(f"<t:{int(time.time())}:F>: {member} was removed from role '{role}'")
 
     @remrole.error
     async def remrole_error(self, ctx, error):
@@ -56,6 +58,8 @@ class admin(commands.Cog):
             await ctx.send("Sorry, I don't have permission to manage roles in this server.")
         elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please include both a user and a role.")
+        else:
+            await ctx.send("I can't manage roles above mine in the server's role hierarchy.")
 
     @commands.command()
     @isLoaded()
@@ -63,10 +67,10 @@ class admin(commands.Cog):
     @commands.bot_has_permissions(kick_members=True)
     async def kick(self, ctx, member : commands.MemberConverter, *, reason = "None"):
         await member.kick(reason = reason)
+        await ctx.send(f"{member} has been kicked.")
         log = self.bot.get_channel(get_log(ctx.guild.id))
         if log:
-            await log.send(f"{member} was kicked by {ctx.author} for reason '{reason}' on <t:{int(time.time())}:F>")
-        await ctx.send(f"{member} has been kicked.")
+            await log.send(f"<t:{int(time.time())}:F>: {member} was kicked by {ctx.author} for reason '{reason}'")
 
     @kick.error
     async def kick_error(self, ctx, error):
@@ -85,10 +89,10 @@ class admin(commands.Cog):
     @commands.bot_has_permissions(ban_members=True)
     async def ban(self, ctx, member : commands.MemberConverter, *, reason = "None"):
         await member.ban(reason = reason)
+        await ctx.send(f"{member} has been banned.")
         log = self.bot.get_channel(get_log(ctx.guild.id))
         if log:
-            await log.send(f"{member} was banned by {ctx.author} for reason '{reason}' on <t:{int(time.time())}:F>")
-        await ctx.send(f"{member} has been banned.")
+            await log.send(f"<t:{int(time.time())}:F>: {member} was banned by {ctx.author} for reason '{reason}'")
 
     @ban.error
     async def ban_error(self, ctx, error):
@@ -99,23 +103,27 @@ class admin(commands.Cog):
         elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please select a user.")
         else:
-            ctx.send("Sorry, I can't do that right now.")
+            await ctx.send("Sorry, I can't do that right now.")
 
     @commands.command()
     @isLoaded()
     @commands.has_permissions(ban_members=True)
     @commands.bot_has_permissions(ban_members=True)
     async def unban(self, ctx, *, member):
+        await ctx.send("1")
         banned_users = await ctx.guild.bans()
+        await ctx.send("2")
         name, discriminator = member.split("#")
+        await ctx.send(name)
+        await ctx.send(discriminator)
         for entry in banned_users:
             user = entry.user
             if(user.name, user.discriminator) == (name, discriminator):
                 await ctx.guild.unban(user)
+                await ctx.send(f"{member} has been unbanned.")
                 log = self.bot.get_channel(get_log(ctx.guild.id))
                 if log:
-                    await log.send(f"{member} was unbanned by {ctx.author} on <t:{int(time.time())}:F>")
-                await ctx.send(f"{member} has been banned.")
+                    await log.send(f"<t:{int(time.time())}:F>: {member} was unbanned by {ctx.author}")
                 return
 
     @unban.error
@@ -127,7 +135,8 @@ class admin(commands.Cog):
         elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please select a user.")
         else:
-            ctx.send("Sorry, I can't do that right now.")
+            print(error)
+            await ctx.send("Sorry, I can't do that right now.")
 
 def setup(bot):
     bot.add_cog(admin(bot))
