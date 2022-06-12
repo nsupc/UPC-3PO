8e664ad2207f591581f2afd1eac5a41e39937ab7
UPC-NS <87896540+UPC-NS@users.noreply.github.com>
2022-06-12 14:08:53 -0400

    started config command

--- cogs/config.py
+++ cogs/config.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 
 from functions import connector,get_log,get_cogs,logerror
 
+'''
+Add config command, shows which cogs are loaded in a server, prefix, log & welcome channels, etc etc
+'''
+
 class config(commands.Cog):
 
     def __init__(self, bot):
@@ -41,6 +45,44 @@ class config(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(administrator=True)
+    async def config(self, ctx):
+        cogs = ['Config']
+
+        mydb = connector()
+        mycursor = mydb.cursor()
+
+        mycursor.execute(f'SELECT * FROM guild WHERE serverid = "{ctx.guild.id}" LIMIT 1')
+        data = mycursor.fetchone()
+        
+        if "n" in data[7]:
+            cogs.append('NSInfo')
+        if "v" in data[7]:
+            cogs.append('Verification')
+        if "a" in data[7]:
+            cogs.append('Admin')
+
+        if len(cogs) > 1:
+            cog = ", ".join(cogs[:-1]) + ', and ' + cogs[-1]
+        else:
+            cog = cogs[0] 
+
+        color = int("2d0001", 16)
+
+        embed = discord.Embed(title=data[1], colour=color)
+        embed.add_field(name="Command Prefix",value=data[3],inline=True)
+        embed.add_field(name="Loaded cogs",value=cog, inline=True)
+        await ctx.send(embed=embed)
+
+        channel = self.bot.get_channel(int(data[4]))
+        print(channel)
+
+    @config.error
+    async def config_error(self, ctx, error):
+        if isinstance(error, commands.MissingPermissions):
+            await ctx.send("You do not have permission to perform that command.")
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
     async def changeprefix(self, ctx, *, prefix):
         mydb = connector()
         mycursor = mydb.cursor()
@@ -52,7 +94,7 @@ class config(commands.Cog):
     async def changeprefix_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("You do not have permission to perform that command.")
-        if isinstance(error, commands.MissingRequiredArgument):
+        elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please select a new bot prefix.")
         else:
             logerror(ctx, error)
@@ -72,7 +114,7 @@ class config(commands.Cog):
     async def log_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("You do not have permission to perform that command.")
-        if isinstance(error, commands.MissingRequiredArgument):
+        elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please select a channel.")
         else:
             logerror(ctx, error)
@@ -109,7 +151,7 @@ class config(commands.Cog):
     async def welcome_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("You do not have permission to perform that command.")
-        if isinstance(error, commands.MissingRequiredArgument):
+        elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please select a channel.")
         else:
             logerror(ctx, error)
@@ -136,7 +178,7 @@ class config(commands.Cog):
     async def addcog_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("You do not have permission to perform that command.")
-        if isinstance(error, commands.MissingRequiredArgument):
+        elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please select a cog.\nAdmin: a, NSinfo: n, Verify: v")
         else:
             logerror(ctx, error)
@@ -163,28 +205,28 @@ class config(commands.Cog):
     async def remcog_error(self, ctx, error):
         if isinstance(error, commands.MissingPermissions):
             await ctx.send("You do not have permission to perform that command.")
-        if isinstance(error, commands.MissingRequiredArgument):
+        elif isinstance(error, commands.MissingRequiredArgument):
             await ctx.send("Please select a cog.\nAdmin: a, NSinfo: n, Verify: v")
         else:
             logerror(ctx, error)
             await ctx.send("Sorry, I can't do that right now.")
 
     @commands.command()
-    async def help(self, ctx):
-        c = get_cogs(ctx.guild.id)
+    async def help(self, ctx, *, msg):
         color = int("2d0001", 16)
 
-        embed = discord.Embed(title="Config", colour=color)
-        embed.add_field(name="changeprefix",value="Changes the bot's server command prefix.\nUsage: !changeprefix [prefix]", inline=False)
-        embed.add_field(name="welcome",value="Designates a welcome channel and sets the server welcome message.", inline=False)
-        embed.add_field(name="log",value="Designates a channel to record the bot's server usage history.\nUsage: !log [channel id]", inline=False)
-        embed.add_field(name="addcog",value="Enables a set of commands in the server.\nUsage: !addcog [letter]", inline=False)
-        embed.add_field(name="remcog",value="Disables a set of commands in the server.\nUsage: !addcog [letter]", inline=False)
-        embed.add_field(name="help",value="Displays information about the commands that are loaded in this server.", inline=False)
-        embed.add_field(name="ping",value="Displays the bot's latency in ms.", inline=False)
-        await ctx.send(embed=embed)
+        if(msg.lower() == "config"):
+            embed = discord.Embed(title="Config", colour=color)
+            embed.add_field(name="changeprefix",value="Changes the bot's server command prefix.\nUsage: !changeprefix [prefix]", inline=False)
+            embed.add_field(name="welcome",value="Designates a welcome channel and sets the server welcome message.", inline=False)
+            embed.add_field(name="log",value="Designates a channel to record the bot's server usage history.\nUsage: !log [channel id]", inline=False)
+            embed.add_field(name="addcog",value="Enables a set of commands in the server.\nUsage: !addcog [letter]", inline=False)
+            embed.add_field(name="remcog",value="Disables a set of commands in the server.\nUsage: !addcog [letter]", inline=False)
+            embed.add_field(name="help",value="Displays information about the commands that are loaded in this server.", inline=False)
+            embed.add_field(name="ping",value="Displays the bot's latency in ms.", inline=False)
+            await ctx.send(embed=embed)
 
-        if "n" in c:
+        elif(msg.lower() == "nsinfo"):
             embed = discord.Embed(title="NSInfo", colour=color)
             embed.add_field(name="nation",value="Displays information about a NationStates nation.\nUsage: !nation [nation]", inline=False)
             embed.add_field(name="endotart",value="Outputs an HTML sheet containing links to every nation in a region not being endorsed by a nation, as listed in the region's Daily Dump.\nUsage: !endotart [nation]", inline=False)
@@ -193,17 +235,18 @@ class config(commands.Cog):
             embed.add_field(name="s2",value="Displays information about the Season 2 Trading Card of a nation.\nUsage: !s2 [nation]", inline=False)
             embed.add_field(name="region",value="Displays information about a NationStates region.\nUsage: !region [region]", inline=False)
             embed.add_field(name="activity",value="Displays a graph showing the most recent login of every nation in a region, as listed in the region's Daily Dump.\nUsage: !activity [region]", inline=False)
+            embed.add_field(name="resolution",value="Displays information about a previous World Assembly resolution.\nUsage: !resolution [GA/SC][resolution number]", inline=False)
             embed.add_field(name="ga",value="Displays information about the at vote General Assembly resolution.", inline=False)
             embed.add_field(name="sc",value="Displays information about the at vote Security Council resolution.", inline=False)
             await ctx.send(embed=embed)
 
-        if "v" in c:
+        elif(msg.lower() == "verification"):
             embed = discord.Embed(title="Verification", colour=color)
             embed.add_field(name="verify",value="Uses the NationStates Verification API to associate nation names with Discord users.\nUsage: !verify [nation]", inline=False)
             embed.add_field(name="id",value="Displays NationStates nations associated with a particular Discord user.\nUsage: !id [user]", inline=False)
             await ctx.send(embed=embed)
 
-        if "a" in c:
+        elif(msg.lower() == "admin"):
             embed = discord.Embed(title="Admin Tools", colour=color)
             embed.add_field(name="kick",value="Kicks a user from the server.\nUsage: !kick [user] [optional reason]", inline=False)
             embed.add_field(name="ban",value="Bans a user from the server.\nUsage: !ban [user] [optional reason]", inline=False)
@@ -211,6 +254,14 @@ class config(commands.Cog):
             embed.add_field(name="addrole",value="Adds a role to a user.\nUsage: !addrole [user] '[role]'", inline=False)
             embed.add_field(name="remrole",value="Removes a role from a user.\nUsage: !remrole [user] '[role]'", inline=False)
             await ctx.send(embed=embed)
+
+        else:
+            await ctx.send("Sorry, that isn't a command set. The options are Config, NSInfo, Verification, and Admin")
+
+    @help.error
+    async def help_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Which set of commands do you need help with? Config, NSInfo, Verification, or Admin")
 
 def setup(bot):
     bot.add_cog(config(bot))
