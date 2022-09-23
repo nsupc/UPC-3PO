99a74c59e06e23c36d6397a519b91f546be65b13
upc <87896540+UPC-NS@users.noreply.github.com>
2022-09-23 18:47:42 -0400

    This should be a fully functional version of the automod listener
    Checks the specified nation for RMB pings, creates discord reports for moderators to handle

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,11 +1,15 @@
+import datetime
 import discord
+import re
 
 from bs4 import BeautifulSoup as bs
 from discord import app_commands
+from discord.app_commands import Choice
 from discord.ext import commands, tasks
 from dotenv import load_dotenv
 
 from the_brain import api_call, connector, format_names
+from views.moderation_view import ModerationView
 
 load_dotenv()
 
@@ -14,23 +18,74 @@ class moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    '''
+    #Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        if not self.check_notices.is_running():
+            self.check_notices.start()
+    '''
+
+    #Checks
+    def isTestServer():
+        async def predicate(ctx):
+            return ctx.guild.id == 1022638031838134312
+        return commands.check(predicate)
+
 #===================================================================================================#
-    # Making this a command for testing, make it a loop when deploying
-    #@tasks.loop(minutes=1)
-    @commands.command()
-    async def check_notices(self, ctx: commands.Context):
+    @tasks.loop(minutes=1)
+    async def check_notices(self):
+        moderation_channel = self.bot.get_channel(1022638032295297124)
+
         notices_data = bs(api_call(url="https://www.nationstates.net/cgi-bin/api.cgi?nation=terrible_purpose&q=notices", mode=2).text, "xml")
 
         for notice in notices_data.find_all("NOTICE"):
-            if notice.find("NEW") and notice.TYPE.text == "RMB" and notice.URL.text[7:17] == "hesperides":
-                message_data = bs(api_call(url=f"https://www.nationstates.net/cgi-bin/api.cgi?region=hesperides&q=messages;limit=1;id={notice.URL.text.split('postid=')[1].split('#')[0]}", mode=1).text, "xml")
+            if notice.find("NEW") and notice.TYPE.text == "RMB" and re.search("(?<==).+?(?=/)", notice.URL.text).group(0) == "hesperides":
+                message_data = bs(api_call(url=f"https://www.nationstates.net/cgi-bin/api.cgi?region=hesperides&q=messages;limit=1;id={re.search('(?<=id=).+?(?=#)', notice.URL.text).group(0)}", mode=1).text, "xml")
 
-                reported_message_id = message_data.MESSAGE.text.split("]")[0].split(";")[1]
+                reported_message_id = re.search("(?<=;)\d+(?=])", message_data.MESSAGE.text).group(0)
+                reported_user = re.search('(?<==).+?(?=;)', message_data.MESSAGE.text).group(0)
 
                 color = int("2d0001", 16)
-                embed = discord.Embed(title="RMB Report", description=f"[Message](https://www.nationstates.net/page=rmb/postid={reported_message_id}) by {format_names(name=message_data.MESSAGE.text.split('=')[1].split(';')[0], mode=2)}, reported by {format_names(name=message_data.NATION.text, mode=2)}", color=color)
+                embed = discord.Embed(title="RMB Report", description=f"<t:{int(datetime.datetime.now().timestamp())}:f>", color=color)
 
-                await ctx.send(embed=embed)
+                embed.add_field(name=f"Reported Message", value=f"[Message](https://www.nationstates.net/page=rmb/postid={reported_message_id}) by [{format_names(reported_user, mode=2)}](https://www.nationstates.net/nation={reported_user})")
+
+                embed.add_field(name=f"Report", value=f"[Message](https://www.nationstates.net/page=rmb/postid={re.search('(?<=id=).+?(?=#)', notice.URL.text).group(0)}) by [{notice.WHO.text}](https://www.nationstates.net/{notice.WHO_URL.text})")
+
+                embed.add_field(name="Reported Message Preview", value="```{}```".format(re.search('(?<=])[\s\S]*(?=\[/q)', message_data.MESSAGE.text).group(0)[:1000]), inline=False)
+
+                view = ModerationView(embed=embed, reported_message_id=reported_message_id, reported_user=reported_user)
+
+                view.message = await moderation_channel.send(embed=embed, view=view)
+#===================================================================================================#
+
+#===================================================================================================#
+    @commands.hybrid_command(name="automod")
+    @commands.has_permissions(administrator=True)
+    @isTestServer()
+    @app_commands.choices(
+        action = [
+            Choice(name="start", value="start"),
+            Choice(name="stop", value="stop")
+        ]
+    )
+    async def automod(self, ctx:commands.Context, action: str):
+        await ctx.defer()
+
+        match action:
+            case "start":
+                if not self.check_notices.is_running():
+                    self.check_notices.start()
+                    await ctx.reply("Automod started.")
+                else:
+                    await ctx.reply("Automod is already running.")
+            case "stop":
+                if self.check_notices.is_running():
+                    self.check_notices.cancel()
+                    await ctx.reply("Automod stopped.")
+                else:
+                    await ctx.reply("Automod already stopped.")
 #===================================================================================================#
 
 async def setup(bot):
