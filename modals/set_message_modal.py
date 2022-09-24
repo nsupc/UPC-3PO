4346eced2292d008ad679da5914ad75139eed402
upc <87896540+UPC-NS@users.noreply.github.com>
2022-09-23 20:46:38 -0400

    Added logging to welcome message set modal

--- modals/set_message_modal.py
+++ modals/set_message_modal.py
@@ -1,6 +1,6 @@
 import discord
 
-from the_brain import connector
+from the_brain import connector, log
 
 from embeds.config_embeds import get_message_embed
 
@@ -23,14 +23,14 @@ class SetMessageModal(discord.ui.Modal, title="Set Region"):
         mycursor = mydb.cursor()
 
         mycursor.execute(f'UPDATE guild SET welcome = "{self.welcome_message.value}" WHERE serverid = "{modal_interaction.guild_id}"')
-                
         mydb.commit()
 
         await self.message.edit(embed=get_message_embed(author_id=modal_interaction.user.id, guild_id=modal_interaction.guild_id))
+        await log(bot=self.bot, id=modal_interaction.guild.id, action=f"The welcome message was set by {modal_interaction.user}")
         await modal_interaction.followup.send(f"Welcome message set.", ephemeral=True)
 
     async def on_error(self, modal_interaction: discord.Interaction, error: Exception) -> None:
-        await modal_interaction.response.send_message('Oops! Something went wrong.', ephemeral=True)
+        await modal_interaction.followup.send('Oops! Something went wrong.', ephemeral=True)
 
     async def on_timeout(self) -> None:
         for child in self.children:
