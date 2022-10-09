55a1cb9972e2d5a8c03c81d9eeb846ca1c388e42
upc <87896540+UPC-NS@users.noreply.github.com>
2022-10-08 21:40:21 -0400

    Added view

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -7,10 +7,10 @@ from bs4 import BeautifulSoup as bs
 from discord import app_commands
 from discord.app_commands import Choice
 from discord.ext import commands, tasks
+from discord.ui import View
 from dotenv import load_dotenv
 
 from the_brain import api_call, connector, format_names
-from views.moderation_view import ModerationView
 
 load_dotenv()
 
@@ -19,6 +19,69 @@ load_dotenv()
 class moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    #Views
+    class ModerationView(View):
+        def __init__(self, embed, reported_message_id, reported_user):
+            super().__init__(timeout=None)
+            self.embed = embed
+            self.reported_message_id = reported_message_id
+            self.reported_user = reported_user
+
+        @discord.ui.button(label="Respond", style=discord.ButtonStyle.success)
+        async def confirm_callback(self, interaction: discord.Interaction, button):
+            await interaction.response.send_modal(moderation.ModerationModal(embed=self.embed, view=self, message=self.message, reported_message_id=self.reported_message_id, reported_user=self.reported_user))
+
+        @discord.ui.button(label="Dismiss", style=discord.ButtonStyle.danger)
+        async def cancel_callback(self, interaction: discord.Interaction, button):
+            self.value = None
+            for child in self.children:
+                child.disabled = True
+
+            self.embed.add_field(name="Status", value="Dismissed")
+            self.embed.add_field(name="Moderator", value=f"<@!{interaction.user.id}>")
+
+            await interaction.response.edit_message(embed=self.embed, view=self)
+            self.stop()
+
+
+    #Modals
+    class ModerationModal(discord.ui.Modal, title="Moderation"):
+        def __init__(self, embed, view, message, reported_message_id, reported_user):
+            super().__init__()
+            self.embed = embed
+            self.view = view
+            #can probably drop the rest of these because they're part of the view, will test when i am less exhausted
+            self.message = message
+            self.reported_message_id = reported_message_id
+            self.reported_user = reported_user
+
+        response_link = discord.ui.TextInput(
+            label="Response",
+            placeholder="Paste the entire link to your response here"
+        )
+
+        async def on_submit(self, modal_interaction: discord.Interaction):
+            await modal_interaction.response.defer()
+
+            mydb = connector()
+            mycursor = mydb.cursor()
+
+            sql = ("INSERT INTO euromoderation (timestamp, reported_message, user, response, moderator) VALUES (%s, %s, %s, %s, %s)")
+            val = (int(datetime.datetime.now().timestamp()), self.reported_message_id, self.reported_user, self.response_link.value, modal_interaction.user.id)
+
+            mycursor.execute(sql, val)
+            mydb.commit()
+
+            self.value = None
+            for child in self.view.children:
+                child.disabled = True
+
+            self.embed.add_field(name="Status", value=f"[Responded]({self.response_link.value})")
+            self.embed.add_field(name="Moderator", value=f"<@!{modal_interaction.user.id}>")
+
+            await modal_interaction.followup.edit_message(message_id=self.message.id, embed=self.embed, view=self.view)
+
 
     #Events
     @commands.Cog.listener()
@@ -56,7 +119,7 @@ class moderation(commands.Cog):
 
                 embed.add_field(name="Reported Message Preview", value="```{}```".format(re.search('(?<=])[\s\S]*(?=\[/q)', message_data.MESSAGE.text).group(0)[:1000]), inline=False)
 
-                view = ModerationView(embed=embed, reported_message_id=reported_message_id, reported_user=reported_user)
+                view = self.ModerationView(embed=embed, reported_message_id=reported_message_id, reported_user=reported_user)
 
                 view.message = await moderation_channel.send(embed=embed, view=view)
 #===================================================================================================#
